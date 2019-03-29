22f89f6ed517c781117a2a1b834adfffa3f7f789
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2019-03-29 14:30:04 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,26 +1,52 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class Client:
+    def __init__(self, name, ID, money):
+        self.name = name
+        self.ID = ID
+        self.money = money
+
+    def input_cash(self, money):
+        more = int(input("How much money would you like to input?"))
+        self.money += more
+
+    def withdrawal_cash(self, money):
+        less = int(input("How much money would you like to withdraw?"))
+        self.money -= less
+
+    def transfer(self, client2):
+        print("You're transfering money from first Client to the second")
+        transf = int(input("How much would you like to transfer?"))
+        self.money = self.money - transf
+        client2.money = client2.money + transf
+        
+class Bank:
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+        self.list_clients = []
+
+    def adding_clients(self, client):
+        self.list_clients.append(client)
+
+    def show_clients(self, list_clients):
+        for client in list_clients:
+            print(client.name)
+            print(client.ID)
+            print(client.money)
+
+
+#new clients
+client1 = Client("Ola", 1, 2000)
+client2 = Client("Ala", 2, 15000)
+client3 = Client("Jurek", 3, 200)
+
+#new bank
+bank1 = Bank("mBank", "Krakow")
+
+bank1.adding_clients(client1)
+bank1.adding_clients(client2)
+bank1.adding_clients(client3)
+
+bank1.show_clients(bank1.list_clients)
+
+
+
